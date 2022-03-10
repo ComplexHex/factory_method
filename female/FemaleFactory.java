5490026539f1c7a43f1aa64636bf97b765b54bb6
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-03-10 20:57:48 +0300

    v4

--- female/FemaleFactory.java
+++ female/FemaleFactory.java
@@ -1,11 +1,13 @@
 package com.javarush.task.task37.task3702.female;
 
+import com.javarush.task.task37.task3702.AbstractFactory;
 import com.javarush.task.task37.task3702.Human;
 import com.javarush.task.task37.task3702.male.KidBoy;
 import com.javarush.task.task37.task3702.male.Man;
 import com.javarush.task.task37.task3702.male.TeenBoy;
 
-public class FemaleFactory {
+public class FemaleFactory implements AbstractFactory {
+
     public Human getPerson(int age) {
         if (age >= 0 && age < 13) {
             return new KidGirl();
@@ -15,4 +17,5 @@ public class FemaleFactory {
         else return new Woman();
 
     }
+
 }
